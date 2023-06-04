910559cf3025e7de23e4526c10faf8f7cf0c11e0
LoganMuscle <134851533+LoganMuscle@users.noreply.github.com>
2023-06-04 09:48:36 +0300

    Update script.js

--- script.js
+++ script.js
@@ -12,30 +12,6 @@ function run() {
 
     index = weight / (height/100)**2;
     body_mass_index.value = Math.floor(index);
-
-    alert(index);
-
-    if (index < 16) {
-        document.getElementById('span').textContent = 'Выраженный дефицит массы тела';
-    }
-    if ( 16 <= index && index < 18.5 ) {
-        document.getElementById('span').textContent = 'Недостаточная (дефицит) масса тела';
-    }
-    if ( 8.5 <= index && index < 25 ) {
-        document.getElementById('span').textContent = 'Норма';
-    }
-    if ( 25 <= index && index < 30 ) {
-        document.getElementById('span').textContent = 'Избыточная масса тела (предожирение)';
-    }
-    if ( 30 <= index && index < 35 ) {
-        document.getElementById('span').textContent = 'Ожирение первой степени';
-    }
-    if (35 <= index && index < 40) {
-        document.getElementById('span').textContent = 'Ожирение второй степени';
-    }
-    else {
-        document.getElementById('span').textContent = 'Ожирение третьей степени (морбидное)';
-    }
 }
 
 let button = document.querySelector('.btn');
